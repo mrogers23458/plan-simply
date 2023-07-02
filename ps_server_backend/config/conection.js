831cff8e29a1cec9c576eea8fbb2b3f390f3a906
Michael Rogers <michael@estatespace.com>
2023-07-02 13:58:12 -0700

    updated to use mongo drivers

--- ps_server_backend/config/conection.js
+++ ps_server_backend/config/conection.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-const mongodb = await mongoose.connect("mongodb://127.0.0.1:27017/plansimply", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongodb = await mongoose.connect(
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/plansimply",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 export default mongodb;
