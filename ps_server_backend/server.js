61f1e03c529943a1bcd38a194fb40c047bf38829
Michael Rogers <michael@estatespace.com>
2023-07-02 14:26:43 -0700

    updated path

--- ps_server_backend/server.js
+++ ps_server_backend/server.js
@@ -22,9 +22,11 @@ const server = new ApolloServer({
 await server.start();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../ps_client_frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(
+      path.join(__dirname, "../ps_client_frontend/build/index.html")
+    );
   });
 }
 
