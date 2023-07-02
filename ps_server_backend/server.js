72777fd19f23308e0ba211b3d7d0bf72df11537c
Michael Rogers <michael@estatespace.com>
2023-07-02 14:24:35 -0700

    updated dirname

--- ps_server_backend/server.js
+++ ps_server_backend/server.js
@@ -5,6 +5,10 @@ import express from "express";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
