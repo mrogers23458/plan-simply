c8f3c22dc93f7574b5f2222fd88f20d5531085c1
KateMcRo <katemcr0@gmail.com>
2023-08-26 15:11:53 -0700

    added redirect when not logged in

--- ps_client_frontend/src/pages/profile/Profile.jsx
+++ ps_client_frontend/src/pages/profile/Profile.jsx
@@ -1,10 +1,14 @@
 /* Framework & CSS */
 import React from "react";
 import "./profile.css";
+import { useAppState } from "../../providers/AppStateProvider";
+import { Navigate } from "react-router-dom";
 
 export default function Profile() {
+  const [{ user }, appStateDispatch] = useAppState();
   return (
     <>
+      {!user && <Navigate to="/" />}
       <p>This is the profile page.</p>
     </>
   );
